8690623904a0789a3adbb58584ff1eab1468cf41
ssolunac <sanela.solunac@endava.com>
2023-02-01 10:23:44 +0100

    Transfer funds Test

---
+++ cypress/integration/e2e_tests/transfer-funds.spec.js
@@ -0,0 +1,31 @@
+describe('Tansfer funds Verification Test', () => {
+	before(function () {
+		cy.visit('http://zero.webappsecurity.com/index.html')
+		cy.url().should('include', 'index.html')
+		cy.get('#signin_button').click()
+
+		cy.fixture('user').then(user => {
+			const username = user.id
+			const password = user.pwd
+
+			cy.login(username, password)
+		})
+		cy.get('ul.nav-tabs').should('be.visible')
+	})
+
+	it('should fill transfer funds form', () => {
+		cy.get('#transfer_funds_tab').click()
+		cy.get('#tf_fromAccountId').select('2')
+		cy.get('#tf_toAccountId').select('4')
+		cy.get('#tf_amount').type(1000)
+		cy.get('#tf_description').type('Just Some Description')
+		cy.get('#btn_submit').click()
+	})
+
+	it('should verify correct data', () => {
+		cy.get('#tf_fromAccountId').should('have.value', 'Checking')
+		cy.get('#tf_toAccountId').should('have.value', 'Loan')
+		cy.get('#tf_amount').should('have.value', 1000)
+		cy.get('#tf_description').should('have.value', 'Just Some Description')
+	})
+})
